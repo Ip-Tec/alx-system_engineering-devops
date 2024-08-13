7d72e6fdd8794d7e2bea219d866bcb55c2275684
Innocent Peter <otakhorpeter@gmail.com>
2024-08-13 15:21:55 +0100

    fix 0-subs v0.0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """Function to query subscribers on a given Reddit subreddit."""
 
+
 import requests
 
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
+
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
