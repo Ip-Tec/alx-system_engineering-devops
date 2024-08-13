199e1af26d0e67e92c50456a5ecce31af3b50b43
Innocent Peter <otakhorpeter@gmail.com>
2024-08-13 20:44:42 +0100

    fit for py 3.6 to py 3.4

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """Function to query subscribers on a given Reddit subreddit."""
 
-
 import requests
 
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
